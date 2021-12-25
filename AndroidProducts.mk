857713e498d4271bc6c300d136246476581170e0
bola980 <mr894212@gmail.com>
2021-12-25 11:58:01 +0000

    sweet: Bringup Corvus-OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/corvus_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    corvus_sweet-eng \
+    corvus_sweet-userdebug \
+    corvus_sweet-user
--- corvus_sweet.mk
+++ corvus_sweet.mk
@@ -5,13 +5,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common CorvusOS configurations
+$(call inherit-product, vendor/corvus/config/common.mk)
 
 # Inherit ANX camera
 $(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
 
-PRODUCT_NAME := arrow_sweet
+PRODUCT_NAME := corvus_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -19,7 +19,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-DEVICE_MAINTAINER := daniml3
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
@@ -27,3 +26,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
+
+#CORVUS
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_GAPPS_ARCH := arm64
+
+#Corvus Maintainer
+CORVUS_MAINTAINER := BolaRamy
+
+#API
+SKIP_ABI_CHECKS := true
+
+#phhone
+IS_PHONE := true scr_resolution := 1080
+TARGET_INCLUDE_WIFI_EXT := true
